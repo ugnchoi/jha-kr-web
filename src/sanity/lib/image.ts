dde44e7faf9a88ea832a038965dfa1513fc17b87
Choi Yoojin <ugn.choi@gmail.com>
2025-12-03 14:44:40 +0900

    fix: use default import imageUrlBuilder for sanity image url

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,10 +1,10 @@
-import createImageUrlBuilder from "@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 import { dataset, projectId } from "../env";
 
 // https://www.sanity.io/docs/image-url
-const builder = createImageUrlBuilder({ projectId, dataset });
+const builder = imageUrlBuilder({ projectId, dataset });
 
 export const urlFor = (source: SanityImageSource) => {
   return builder.image(source);
